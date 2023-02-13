23b5761908cc4a1dcaa976680adb0d8e26358c24
aris402 <aris4455@hotmail.com>
2023-02-12 22:33:29 -0300

    Showing projects on screen

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,13 @@
 import Message from "../layout/Message"
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import styles from './Projects.module.css'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import ProjectCard from "../project/ProjectCard"
+import { useState, useEffect } from "react"
 
 function Projects(){
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = ''
@@ -9,10 +15,36 @@ function Projects(){
         message = location.state.message
     }
 
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(data => {
+            setProjects(data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
     return(
         <div>
+            <div>
             <h1>Meus projetos</h1>
+            <LinkButton to="newproject" text="Criar projeto"/>
+            </div>
             {message && <Message msg={message} type="success"/>}
+            <Container customClass="start">
+                {projects.length > 0 &&
+                  projects.map((project) =>
+                  <ProjectCard
+                  name={project.name}
+                  id={project.id}
+                  key={project.id}
+                  budget={project.budget}
+                  category={project.category.name}/>)}
+            </Container>
         </div>
     )
 }
---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,19 @@
+import styles from './ProjectCard.module.css'
+import {BsPencil, BsFillTrashFill} from  'react-icons/bs'
+
+function ProjectCard({id, name, budget, category, handleRemove}){
+    return(
+        <div className={styles.project_card}>
+        <h4>{name}</h4>
+        <p>
+            <span>Orçamento:</span> R${budget}
+        </p>
+        <div>
+            <p>Editar</p>
+            <p>Remover</p>
+        </div>
+        </div>
+    )
+}
+
+export default ProjectCard
